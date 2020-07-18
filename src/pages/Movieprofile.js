a65796ef76da6acd4ff780d6a16527b600668cf2
Ray H <rjhern37@neiu.edu>
2020-07-18 13:36:08 -0500

    removed popup

---
+++ src/pages/Movieprofile.js
@@ -0,0 +1,18 @@
+import React from 'react'
+import Navbar from "../components/Navbar"
+
+function Movieprofile() {
+    return (
+        <div>
+              <section>
+            <Navbar />
+            </section>
+            <section>
+                
+            </section>
+            
+        </div>
+    )
+}
+
+export default Movieprofile
